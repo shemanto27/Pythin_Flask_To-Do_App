4f7209498f901d0cc921d0a231f7d71193274f19
shemanto27 <shemantosharkarofficial@gmail.com>
2024-07-28 23:30:24 +0600

    route done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -13,7 +13,7 @@ db = SQLAlchemy(app)                                              #Initializing 
 # Making the table
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
+    task = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
 
@@ -23,6 +23,29 @@ class Todo(db.Model):
 def home():
     todo_list = Todo.query.all()
     return render_template("base.html", todo_list=todo_list)
+
+@app.route('/add', methods=["POST"])
+def add():
+    task = request.form.get("task")
+    new_todo = Todo(task=task, complete= False) #A new Todo object is created
+    db.session.add(new_todo) #adds the new to-do item to the current database session
+    db.session.commit() #saving the new to-do item to the database
+    return redirect(url_for("home"))
+
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
 
 if __name__ == '__main__':
     with app.app_context():
